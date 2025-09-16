b7130889c90e7de38a5527d428fa3993543aa504
Nhi Huynh <nhi.huynh@hotmail.com>
2025-09-16 18:08:15 +0800

    feat: fetch with promises

--- src/index.js
+++ src/index.js
@@ -37,8 +37,32 @@ console.log("Page loaded!");
 
 
 
+// Make API requests to this endpoint to get Pokemon data:
+// https://pokeapi.co/api/v2/pokemon/
+
+function getPokemonData(targetPokemon){
+	let result = fetch("https://pokeapi.co/api/v2/pokemon/" + targetPokemon).then((response) => {
+		console.log(response);
+		// If the response body contains JSON data, return that as a valid JSON object
+		return response.json();
+	}).then((jsonResponse) => {
+		// Logging the object with a string requires us to convert the object into a string manually:
+		console.log("jsonResponse is: \n" + JSON.stringify(jsonResponse, null, 4));
+		// Logging the object directly shows the object in full:
+		// console.log(jsonResponse);
+		return jsonResponse;
+	});
 
 
-// favouritePokemon.forEach((i) => {
+	console.log(result);
+	setTimeout(() => {
+		console.log(result);
+	}, 2000);
 
-// });
+}
+
+// function add(num1, num2){
+// 	return num1 + num2;
+// }
+
+getPokemonData("pikachu");
