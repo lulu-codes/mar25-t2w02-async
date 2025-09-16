cc6ed3eadfb139388bee41b126031b345ed01eca
Nhi Huynh <nhi.huynh@hotmail.com>
2025-09-16 17:46:17 +0800

    feat!: project init with callback examples

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mar25-t2w02-async
+
+Repo showing the different JS ways of handling async functionality.
+
+Goals:
+
+- callbacks
+  - setTimeout
+- promises
+  - sequential promises
+  - parallel promises
+- fetch
+- async/await syntax
---
+++ src/index.js
@@ -0,0 +1,44 @@
+
+
+// (() => {
+
+// })
+
+// someArray.forEach(() => {})
+
+// someArray.forEach(someFunctionThatHandlesArrayStuff())
+
+
+let favouritePokemon = [
+	"haunter",
+	"tyranitar",
+	"gengar",
+	"snorlax",
+	"jigglypuff",
+	"bulbasaur",
+	"hitmonlee",
+	"blastoise",
+	"pikachu"
+];
+
+favouritePokemon.forEach((individualName) => {
+	console.log("This is a super cool Pokemon: " + individualName)
+});
+
+
+// setTimeout(() => {})
+
+setTimeout(() => {
+	console.log("Subscribe to the newsletter!");
+}, 1000 * 5);
+
+
+console.log("Page loaded!");
+
+
+
+
+
+// favouritePokemon.forEach((i) => {
+
+// });
